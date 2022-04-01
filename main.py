7539579d748a35070234838bb96b43a8add9f64e
spongxin <2624490979@qq.com>
2022-04-01 22:44:47 +0800

    Trained model VGGNet with input<12, 75, 3>

--- main.py
+++ main.py
@@ -1,41 +1,48 @@
 from tensorflow import keras
 from detect import FrameMap
+from sklearn.cluster import KMeans
 import numpy as np
 import os
 
 
 class Solution(object):
-    def __init__(self, model: str, interval: int = 1):
-        self.coordinates = None
+    def __init__(self, model: str = 'VGGNet15', interval: int = 1, min_detection=0.8, min_tracking=0.4):
+        self.coords = None
         self.landmarks = None
         self.frames = None
         self.model = model
         self.interval = interval
-        self.selected = 16
+        self.clusters = 16
+        self.kmeans = KMeans(n_clusters=self.clusters)
+        self.min_detection = min_detection
+        self.min_tracking = min_tracking
 
     def process(self, video: str):
-        frame_map = FrameMap()
-        self.frames = frame_map.video2frames(video, self.interval)
-        print('frames 0', self.frames.shape)
-        self.landmarks = np.array([[*frame_map.frame2landmarks(f)] for f in self.frames])
-        print('landmarks', self.landmarks.shape)
-        coordinates, ignored = [], []
+        fm = FrameMap(
+                min_detection_confidence=self.min_detection,
+                min_tracking_confidence=self.min_tracking
+        )
+        self.frames = fm.video2frames(video, self.interval)
+        self.landmarks = np.array([[*fm.frame2landmarks(f)] for f in self.frames])
+        coords, dropped = [], []
         for index, landmark in enumerate(self.landmarks):
             if landmark[0] is not None and landmark[1] is not None:
-                coordinates.append(frame_map.landmarks2value(*landmark))
+                coords.append(fm.landmarks2value(*landmark))
                 continue
-            ignored.append(index)
-        print('ignored', ignored)
-        self.coordinates = np.array(coordinates)
-        print('coordinates', self.coordinates.shape)
-        self.frames = frame_map.remove_frames(self.frames, np.array(ignored)) if ignored else self.frames
-        print('frames 1', self.frames.shape)
-        frame_map.close()
+            dropped.append(index)
+        self.coords = fm.remove_frames(np.array(coords), np.array(dropped)) if dropped else np.array(coords)
+        self.frames = fm.remove_frames(self.frames, np.array(dropped)) if dropped else self.frames
+
+    def select_key_frames(self):
+        if not self.coords:
+            return
+        self.kmeans.fit(self.coords)
+        return self.kmeans.predict(self.coords)
 
     def predict(self):
-        if self.coordinates.shape[0] < self.selected:
+        if self.coords.shape[0] < self.clusters:
             return
-        X = [self.coordinates[np.linspace(0, self.coordinates.shape[0]-1, self.selected).astype(int)], ]
+        X = [self.coords[np.linspace(0, self.coords.shape[0] - 1, self.clusters).astype(int)], ]
         X = np.array(X, dtype='float32')
         return self.get_model(self.model).predict(X)
 
@@ -48,8 +55,9 @@ class Solution(object):
 if __name__ == '__main__':
     s = Solution(model='VGGNet15', interval=1)
     s.process("dataset/valid/003_001_003.mp4")
-    print(s.coordinates.shape)
+    print(s.coords.shape)
     ret = s.predict()
     print(ret)
     print(np.argmax(ret, axis=1)+1)
+    print(s.select_key_frames())
 
