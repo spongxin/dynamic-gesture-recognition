ade721b01f40b9ddfca7951aa2dce4d44c381607
dell <442417060@qq.com>
2022-04-01 23:01:10 +0800

    更新动图-syp

--- drawer.py
+++ drawer.py
@@ -1,5 +1,6 @@
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from  matplotlib import animation
 import numpy as np
 
 
@@ -24,27 +25,32 @@ class Drawer:
                 plt.plot(x, y)
         plt.show()
 
-    def draw_body(self, frame: int = 0):
+    def draw_body(self, frame: int = 0,flag:int=0,ax=0):
         """画身体骨骼3d图
         :param frame: 视频下标帧序号
+        :param flag: flag!=0时为画动图，不需要show()
         :return: none
         """
-        fig = plt.figure()
-        ax = Axes3D(fig)
+        if flag==0:
+            fig = plt.figure()
+            ax = Axes3D(fig)
         self._draw3d(ax, [8, 6, 5, 4, 0, 1, 2, 3, 7], frame)
         self._draw3d(ax, [23, 11, 12, 24], frame)
         self._draw3d(ax, [12, 14, 16], frame)
-        self._draw3d(ax, [11, 13, 15], frame)
-        plt.show()
+        line=self._draw3d(ax, [11, 13, 15], frame)
+        if flag==0:
+            plt.show()
+        return line
 
-    def draw_hand(self, mode: int = 0, frame: int = 0):
+    def draw_hand(self, mode: int = 0, frame: int = 0,flag: int=0,ax=0):
         """画手部骨骼3d图
         :param frame: 视频下标帧序号
         :param mode: 左手0/右手1
         :return:
         """
-        fig = plt.figure()
-        ax = Axes3D(fig)
+        if flag==0:
+            fig = plt.figure()
+            ax = Axes3D(fig)
         base = 21 * mode
         # 大拇指
         tmp = np.array([33, 34, 35, 36, 37]) + base
@@ -63,20 +69,49 @@ class Drawer:
         self._draw3d(ax, tmp, frame)
         # 小拇指
         tmp = np.array([33, 50, 51, 52, 53]) + base
-        self._draw3d(ax, tmp, frame)
+        line=self._draw3d(ax, tmp, frame)
+        if flag==0:
+            plt.show()
+        return line
+    def draw_ani(self,intervals:int=200,mod:int=0):
+        """画身体骨骼3d图
+            :param frame: 播放每帧的切换速度
+            :param mod: 0/左手,1/右手,其他/身体,默认为左手0
+            :return: none
+        """
+        fig = plt.figure()
+        ax = Axes3D(fig)
+        def ani_func(i):
+            ax.cla()
+            if mod==0 or mod==1:
+                line=self.draw_hand(mod,i%data.shape[0],1,ax)
+            else:
+                line=self.draw_body(i%data.shape[0],1,ax)
+            return line
+        def init():
+            if mod == 0 or mod == 1:
+                line=self.draw_hand(0,0,1,ax)
+            else:
+                line = self.draw_body(0, 1, ax)
+            return line
+        ani = animation.FuncAnimation(fig, func=ani_func, frames=100, init_func=init, interval=intervals, blit=False)
         plt.show()
-
     @staticmethod
     def normalization(X):
         _range = np.max(X) - np.min(X)
         return (X - np.min(X)) / _range
 
     def _draw3d(self, ax, index, frame: int = 0):
+        """
+        :param index: 位点
+        :param frame:视频索引下标
+        :return: 只有画动图里面用到
+        """
         x, y, z = self.coordinates[frame, index, 0], 1-self.coordinates[frame, index, 1], self.coordinates[frame, index, 2]
-        ax.plot3D(x, z, y, color='red', alpha=0.5, linestyle='--', linewidth=2)
-
+        line=ax.plot3D(x, z, y, color='red', alpha=0.5, linestyle='--', linewidth=2)
+        return  line
 
 if __name__ == '__main__':
     data = np.load('dataset/NPZ/001_009_004.npz')['y']
     print(data.shape)
-    Drawer(data).draw_hand(frame=5)
+    Drawer(data).draw_ani(200,mod=2)
