875d2acbdf8caa703de4283df6aacb37a7e1e78c
spongxin <2624490979@qq.com>
2022-04-03 11:55:13 +0800

    Changed Ui_MainWindow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import numpy as np
+from ui.window import Ui_MainWindow
 from resource import Solution
+from PyQt5 import QtWidgets
+import numpy as np
+import sys
 import os
 
 
@@ -9,13 +12,14 @@ labels = ["Opaque", "Red", "Green", "Yellow", "Bright", "Light-blue", "Colors", 
           "Yogurt", "Accept", "Thanks", "Shut down", "Appear", "To land", "Catch", "Help", "Dance", "Bathe", "Buy", "Copy", "Run", "Realize", "Give", "Find"]
 
 if __name__ == '__main__':
-    video = "dataset/LSA64/005_001_003.mp4"
-    s = Solution(model=os.path.join(os.getcwd(), 'model', 'VGGNet12'), clusters=12)
-    s.process(video)
-    print(s.coords.shape)
-    ret = s.predict()
-    if ret is not None and ret.size:
-        ret = ret[0]
-        for k in np.argsort(ret)[::-1][:5]:
-            print(f"Type: {k+1}, Probability: {ret[k]}")
-        print(f"The result of {video} is {np.argmax(ret)+1}, {labels[np.argmax(ret)]}")
+    app = QtWidgets.QApplication(sys.argv)
+    window = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(window)
+    window.show()
+    sys.exit(app.exec_())
+    # video = "dataset/valid/003_001_001.mp4"
+    # s = Solution(model=os.path.join(os.getcwd(), 'model', 'VGGNet12'), clusters=12, min_detection=0.8)
+    # s.process(video)
+    # print(s.coords.shape)
+
